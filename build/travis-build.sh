a53308b29c0a50cabaeca3b8cf04e84d58fb6901
Nikolay Zapolnov <zapolnov@gmail.com>
2015-06-20 18:13:02 +0300

    Continued travis experiments.

--- build/travis-build.sh
+++ build/travis-build.sh
@@ -2,12 +2,20 @@
 
 set -e
 
+travis_dir=`pwd`
+
+cleanup {
+    cd "$travis_dir"
+}
+trap cleanup EXIT INT QUIT HUP TERM
+
 cd `dirname "$0"`
 
 mkdir -p ".cmake-$Z_TARGET_PLATFORM-$CMAKE_BUILD_TYPE"
 cd ".cmake-$Z_TARGET_PLATFORM-$CMAKE_BUILD_TYPE"
 
 cmake \
+    "-DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH" \
     "-DZ_TARGET_PLATFORM=$Z_TARGET_PLATFORM" \
     "-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE" \
     ../..
