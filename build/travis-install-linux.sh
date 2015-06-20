7eb58d277d70e53168294e8c2608362f27bfb37e
Nikolay Zapolnov <zapolnov@gmail.com>
2015-06-20 18:36:06 +0300

    Travis again.

--- build/travis-install-linux.sh
+++ build/travis-install-linux.sh
@@ -4,8 +4,8 @@ set -e
 
 # Install CMake
 wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-i386.tar.gz
-tar -xvzf cmake-3.2.2-Linux-i386.tar.gz
-sudo cp -fR cmake-3.2.2-Linux-i386/* /usr
+tar -xzf cmake-3.2.2-Linux-i386.tar.gz
+sudo cp -fRv cmake-3.2.2-Linux-i386/* /usr
 
 # Add PPA with Qt 5.4
 sudo apt-add-repository -y ppa:beineri/opt-qt541
