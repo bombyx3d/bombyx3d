bf72692daef7b39bfc5b2dbc5a8f4af6f99e5f4d
Nikolay Zapolnov <zapolnov@gmail.com>
2015-06-20 19:03:41 +0300

    Travis GCC fix.

--- build/travis-build.sh
+++ build/travis-build.sh
@@ -13,6 +13,14 @@ cd `dirname "$0"`
 mkdir -p .cmake-build
 cd .cmake-build
 
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+    if [ "${CC}" = "gcc" ]; then export CC=gcc-4.8; fi
+    if [ "${CXX}" = "g++" ]; then export CXX=g++-4.8; fi
+fi
+
+echo CC=${CC}
+echo CXX=${CXX}
+
 /usr/bin/cmake \
     "-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE" \
     "-DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH" \
--- build/travis-install-linux.sh
+++ build/travis-install-linux.sh
@@ -8,9 +8,6 @@ sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 sudo apt-get -qq install g++-4.8 libc6-i386 qt54base qt54multimedia qt54tools
 
-if [ "$CC" = "gcc" ]; then export CC=gcc-4.8; fi
-if [ "$CXX" = "g++" ]; then export CXX=g++-4.8; fi
-
 wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-i386.tar.gz
 tar -xzf cmake-3.2.2-Linux-i386.tar.gz
 sudo cp -fR cmake-3.2.2-Linux-i386/* /usr
